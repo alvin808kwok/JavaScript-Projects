1b11c35395e71975026cce118e47ba83a7670f30
Alvin Kwok <alvin808kwok@gmail.com>
2019-09-10 10:57:07 -1000

    Basic JavaScript

--- Basic JavaScript Projects/JS/Basic_JavaScript_2.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -1,17 +1,17 @@
 function mySecondFunction() {
-    var str = "Highlighted Yellow";
+    var str = "Highlighted Text";
     var result = str.fontcolor("yellow");
     document.getElementById("Yellow_Text").innerHTML = result;
 } 
 
-// function myFunction() {
-//     var sentence = "I am learning";
-//     sentence += " a lot from this book!";
-//     document.getElementById("Concatenate").innerHTML = sentence;
-// }
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
 
-// function addition_Function() {
-//     var addition = 2 + 2;
-//     document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
-// }
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
 
--- Basic JavaScript Projects/JS/Basic_JavaScript_3.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_3.js
@@ -1,5 +1,47 @@
 function addition_Function() {
     var addition = 2 + 2;
-    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+    document.getElementById("addition").innerHTML = "2 + 2 = " + addition;
 }
 
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("subtraction").innerHTML = "5 - 2 = " + subtraction;
+}
+
+function multiplication() {
+    var multiply = 6 * 8;
+    document.getElementById("multiply").innerHTML = "6 x 8 = " + multiply;
+}
+
+function division() {
+    var division = 48 / 6;
+    document.getElementById("division").innerHTML = "48 / 6 = " + division;
+}
+
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 - 5;
+    document.getElementById("PEMDAS").innerHTML = "(1 + 2) * 10 / 2 - 5 = " + simple_Math;
+}
+
+function modulus_Operator() {
+    var modulus = 25 % 6;
+    document.getElementById("modulus").innerHTML = "What is the remainer of 25 divided by 6? Answer: " + modulus;
+}
+
+function negation_Operator() {
+    var x = 10;
+    document.getElementById("negative").innerHTML = "What is the negative number for 10? Answer: " + -x;
+}
+
+var X = 5;
+X++;
+document.write(X + ", ");
+
+var Y = 5.25;
+Y--;
+document.write(Y + ", ");
+
+// Random number between 1 and 0
+window.alert(Math.random());
+// Random number between 0 and 100
+window.alert(Math.random() * 100);
