51466bbf16f5543bf684290a338ae02e6c2b6d8b
Alvin Kwok <alvin808kwok@gmail.com>
2019-09-09 20:51:20 -1000

    Basic JavaScript

--- Basic JavaScript Projects/JS/Basic_JavaScript_2.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -4,9 +4,14 @@ function mySecondFunction() {
     document.getElementById("Yellow_Text").innerHTML = result;
 } 
 
-function myFunction() {
-    var sentence = "I am learning";
-    sentence += " a lot from this book!";
-    document.getElementById("concatenate").innerHTML = sentence;
-}
+// function myFunction() {
+//     var sentence = "I am learning";
+//     sentence += " a lot from this book!";
+//     document.getElementById("Concatenate").innerHTML = sentence;
+// }
+
+// function addition_Function() {
+//     var addition = 2 + 2;
+//     document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+// }
 
