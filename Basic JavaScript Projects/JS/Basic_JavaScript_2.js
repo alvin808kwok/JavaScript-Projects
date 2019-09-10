6d19c789c1573321548398c01ea17450619b91d8
Alvin Kwok <alvin808kwok@gmail.com>
2019-09-09 16:20:58 -1000

    Basic JavaScript 2
    Add function mySecondFunction and function myFunction. mySecondFunction is a button when clicked on turns yellow. myFunction is a paragraph when clicked on returns a string.

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,12 @@
+function mySecondFunction() {
+    var str = "Highlighted Yellow";
+    var result = str.fontcolor("yellow");
+    document.getElementById("Yellow_Text").innerHTML = result;
+} 
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
+
