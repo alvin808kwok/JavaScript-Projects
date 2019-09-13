b3d9e87a6937bb33a9c2ef46d2d2b98101d4b8fc
Alvin Kwok <alvin808kwok@gmail.com>
2019-09-12 15:54:41 -1000

    Basic JavaScript 9

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_9.js
@@ -0,0 +1,14 @@
+// Creating a JavaScript Program - timer that will countdown by seconds.
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds -1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+    if(seconds == -1){
+        alert("Time's up!");
+    }
+        }
+    tick();
+}
